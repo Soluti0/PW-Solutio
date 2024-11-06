0a4cf708dc2e571936e2b13c2489f9082d12ab64
guelcesar <144190709+guelcesar@users.noreply.github.com>
2024-11-06 21:02:04 +0000

    Se integran cambios de chatbox

---
+++ js/chat-container.js
@@ -0,0 +1,8 @@
+function toggleChat() {
+    const chatContainer = document.getElementById("chatContainer");
+    if (chatContainer.style.display === "none" || chatContainer.style.display === "") {
+        chatContainer.style.display = "block";
+    } else {
+        chatContainer.style.display = "none";
+    }
+}
